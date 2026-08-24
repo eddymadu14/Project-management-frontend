1f09721472e67841e98fb26512c48a028586b838
eddymadu14 <edwardmaduneme.com>
2026-08-24 22:49:04 +0100

    dirty commit

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 // Create a reusable Axios instance
 const api = axios.create({
-  baseURL: `{import.meta.env.VITE_API_URL}/api`, // your backend base URL
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // your backend base URL
   headers: {
     "Content-Type": "application/json",
   },
